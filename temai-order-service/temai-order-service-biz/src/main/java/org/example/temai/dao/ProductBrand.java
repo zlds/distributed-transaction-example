2fa64dc9ada41886558462fdb6c138b26cb5305f
hanchaowei <hanchaowei0@gmail.com>
2023-10-24 20:11:01 +0800

    商品、订单dao接口添加

---
+++ temai-order-service/temai-order-service-biz/src/main/java/org/example/temai/dao/ProductBrand.java
@@ -0,0 +1,12 @@
+package org.example.temai.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author: hanchaowei
+ * @date 2023/10/24
+ * @description:
+ */
+
+public interface ProductBrand extends BaseMapper<ProductBrand> {
+}
---
+++ temai-order-service/temai-order-service-biz/src/main/java/org/example/temai/dao/ProductReviewMapper.java
@@ -0,0 +1,13 @@
+package org.example.temai.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.temai.domain.ProductReview;
+
+/**
+ * @author: hanchaowei
+ * @date 2023/10/24
+ * @description:
+ */
+
+public interface ProductReviewMapper extends BaseMapper<ProductReview> {
+}
